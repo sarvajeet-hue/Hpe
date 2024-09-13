df2d03e7e89dd5d2414dbdaaf60305b324705ef9
sarvajeet-hue <sarvajeet.1923en1095@kiet.edu>
2024-09-13 18:26:11 +0530

    first commit

--- src/Components/ClickPhoto.jsx
+++ src/Components/ClickPhoto.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
+
 export const ClickPhoto = () => {
+
+    
   return (
-    <div>ClickPhoto</div>
+    <div className='flex items-center justify-center '> 
+        <div>
+            <img className='h-[500px] w-[500px]' src="https://www.shutterstock.com/shutterstock/photos/1554086789/display_1500/stock-photo-close-up-portrait-of-yong-woman-casual-portrait-in-positive-view-big-smile-beautiful-model-posing-1554086789.jpg" alt="" />
+        </div>
+    </div>
   )
 }
