b5d8a5c0f02a67d5a86708f7977325851fef49d5
sarvajeet-hue <sarvajeet.1923en1095@kiet.edu>
2024-09-16 18:54:02 +0530

    Updated code

--- src/Components/IndustrySelection.jsx
+++ src/Components/IndustrySelection.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { PrimaryButton } from './PrimaryButton/PrimaryButton'
+import { useNavigate } from 'react-router-dom'
 
 export const IndustrySelection = () => {
+
+  const navigate = useNavigate()
   return (
     <div className='flex items-center justify-center gap-6'>
-        <PrimaryButton linkto={'/clickphoto'}>Compute</PrimaryButton>
-        <PrimaryButton >Storage</PrimaryButton>
+        <PrimaryButton onClick={() =>navigate('/clickphoto')}>Compute</PrimaryButton>
+        <PrimaryButton onClick={() =>navigate('/clickphoto')} >Storage</PrimaryButton>
     </div>
   )
 }
